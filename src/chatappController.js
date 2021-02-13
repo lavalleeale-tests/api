b101e5b95324f2e113f2721707d8fc02631a9cb3
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2021-02-13 08:26:50 -0800

    Fixed multiline whispers

--- src/chatappController.js
+++ src/chatappController.js
@@ -14,15 +14,15 @@ module.exports = function (app, server) {
         socket.on('sendMessage', (data) => {
             if (data.split(" ")[1] === "/w") {
                 if (data.split(" ").length<3) {
-                    io.to(socket.id).emit("update", `Incorrect Arguemnt Count`)
+                    return io.to(socket.id).emit("update", `Incorrect Arguemnt Count`)
                 }
                 var userTo = Object.keys(allClients).find(key => allClients[key] === data.split(" ")[2])
                 if (!userTo) {
-                    io.to(socket.id).emit("update", `User ${data.split(" ")[2]} not found, maybe a spelling mistake?`)
+                    return io.to(socket.id).emit("update", `User ${data.split(" ")[2]} not found, maybe a spelling mistake?`)
                 }
                 var userFrom = allClients[socket.id]
-                io.to(userTo).emit("update", `Whisper from ${userFrom}: ${data.split(" ").slice(2)}`)
-                io.to(socket.id).emit("update", `Whisper to ${allClients[userTo]}: ${data.split(" ").slice(2)}`)
+                io.to(userTo).emit("update", `Whisper from ${userFrom}: ${data.split(" ").slice(3).join(" ")}`)
+                io.to(socket.id).emit("update", `Whisper to ${allClients[userTo]}: ${data.split(" ").slice(3).join(" ")}`)
             } else {
                 console.log(`Got message: ${data}`)
                 io.to(Array.from(socket.rooms)[1]).emit("update", data)
