e9105eb3f442cd498da3962bbdb5e9b19689a708
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2021-02-13 19:30:04 -0800

    Refactorred all clients list for chatapp

--- src/chatappController.js
+++ src/chatappController.js
@@ -17,50 +17,53 @@ module.exports = (app, server) => {
         if (data.split(' ').length < 4) {
           return io.to(socket.id).emit('update', 'Incorrect Argument Count\n');
         }
-        const userTo = Object.keys(allClients).find((key) => allClients[key] === data.split(' ')[2]);
+        const userTo = Object.keys(allClients).find((key) => allClients[key].username === data.split(' ')[2]);
         if (!userTo) {
           return io.to(socket.id).emit('update', `User ${data.split(' ')[2].replace('\n', '')} not found, maybe a spelling mistake?\n`);
         }
-        const userFrom = allClients[socket.id];
+        const userFrom = allClients[socket.id].username;
         io.to(userTo).emit('update', `Whisper from ${userFrom}: ${data.split(' ').slice(3).join(' ')}`);
-        return io.to(socket.id).emit('update', `Whisper to ${allClients[userTo]}: ${data.split(' ').slice(3).join(' ')}`);
+        return io.to(socket.id).emit('update', `Whisper to ${allClients[userTo].username}: ${data.split(' ').slice(3).join(' ')}`);
       }
       console.log(`Got message: ${data}`);
       return io.to(Array.from(socket.rooms)[1]).emit('update', data);
     });
     socket.on('newUser', (data) => {
       const parsedData = JSON.parse(data);
-      allClients[socket.id] = parsedData.username;
+      allClients[socket.id] = {
+        username: parsedData.username,
+        room: parsedData.room,
+      };
       console.log(`${parsedData.username} has joined with room ${parsedData.room}`);
       socket.join(parsedData.room);
       io.to(Array.from(socket.rooms)[1]).emit('newUser', parsedData.username);
     });
-    socket.on('disconnect', () => {
-      console.log(`${allClients[socket.id]} has left`);
-      io.to(Array.from(socket.rooms)[1]).emit('delUser', allClients[socket.id]);
+    socket.on('disconnecting', () => {
+      console.log(`${allClients[socket.id].username} has left`);
+      io.to(Array.from(socket.rooms)[1]).emit('delUser', allClients[socket.id].username);
       delete (allClients[socket.id]);
     });
     socket.on('changeName', (data) => {
       const parsedData = JSON.parse(data);
       console.log(`${parsedData.oldName} has changed their name to ${parsedData.newName}`);
-      allClients[socket.id] = parsedData.newName;
+      allClients[socket.id].username = parsedData.newName;
       io.to(Array.from(socket.rooms)[1]).emit('changeName', data);
     });
     socket.on('changeRoom', (data) => {
-      console.log(`${allClients[socket.id]} has changed their room to ${data}`);
-      io.to(Array.from(socket.rooms)[1]).emit('delUser', allClients[socket.id]);
+      console.log(`${allClients[socket.id].username} has changed their room to ${data}`);
+      io.to(Array.from(socket.rooms)[1]).emit('delUser', allClients[socket.id].username);
       socket.leave(Array.from(socket.rooms)[1]);
       socket.join(data);
-      io.to(Array.from(socket.rooms)[1]).emit('newUser', allClients[socket.id]);
+      io.to(Array.from(socket.rooms)[1]).emit('newUser', allClients[socket.id].username);
     });
     socket.on('changeInfo', (data) => {
       const parsedData = JSON.parse(data);
-      console.log(`${allClients[socket.id]} has changed their room to ${parsedData.room} and name to ${parsedData.username}`);
+      console.log(`${allClients[socket.id].username} has changed their room to ${parsedData.room} and name to ${parsedData.username}`);
       io.to(Array.from(socket.rooms)[1]).emit('delUser', allClients[socket.id]);
       socket.leave(Array.from(socket.rooms)[1]);
       socket.join(parsedData.room);
-      allClients[socket.id] = parsedData.username;
-      io.to(Array.from(socket.rooms)[1]).emit('newUser', allClients[socket.id]);
+      allClients[socket.id] = parsedData;
+      io.to(Array.from(socket.rooms)[1]).emit('newUser', allClients[socket.id].username);
     });
   });
 };
